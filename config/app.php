c310c207dba4681df51052ed53c04d39db96cde4
root <root@server1.betra.game>
2025-12-04 17:56:31 +0000

    fix

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
 //        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
         App\Providers\WSocketServiceProvider::class,
